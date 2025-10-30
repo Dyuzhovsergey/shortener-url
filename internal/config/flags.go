8941009b8f5bf196d61af7eec548b8a62d81ed3b
Sergey <dyuzhovsergey@gmail.com>
2025-10-30 19:34:35 +0300

    add config

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -7,6 +7,6 @@ import (
 var FlagRunAddr string
 
 func ParseFlags() {
-	flag.StringVar(&FlagRunAddr, "a", ":8080", " port to run server")
+	flag.StringVar(&FlagRunAddr, "a", ":8080", " host and port to run server")
 	flag.Parse()
 }
