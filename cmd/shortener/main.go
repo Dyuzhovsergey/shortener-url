a06197efb5bc22736c2d3ed4fbd9489e6e468370
Sergey <dyuzhovsergey@gmail.com>
2025-11-30 21:46:58 +0300

    add dependence github.com/jackc/pgx/v5 in go.mod

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"log"
 	"net/http"
 
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"go.uber.org/zap"
 
 	"github.com/Dyuzhovsergey/shortener-url/internal/config"
@@ -15,7 +16,7 @@ import (
 )
 
 func main() {
-	// парсим флаги (-a и -b)
+	// парсим флаги
 	cfg := config.Load()
 
 	// инициализируем zap logger
@@ -23,26 +24,40 @@ func main() {
 	defer zapLogger.Sync()
 
 	var repo repository.Repository
+
 	if cfg.FileStoragePath != "" {
 		fileRepo, err := repository.NewFileRepository(cfg.FileStoragePath)
 		if err != nil {
-			log.Fatalf("cannot init file repository: %v", err)
+			zapLogger.Fatal("cannot init file repository: %v", zap.Error(err))
 		}
 		repo = fileRepo
 		zapLogger.Info("using file repository", zap.String("path", cfg.FileStoragePath))
 	} else {
 		repo = repository.NewMemoryRepository()
 		zapLogger.Info("using in-memory repository")
+
+	}
+
+	var (
+		db  *sql.DB
+		err error
+	)
+
+	if cfg.DatabaseDSN != "" {
+		db, err = sql.Open("pgx", cfg.DatabaseDSN)
+		if err != nil {
+			zapLogger.Fatal("failed to open DB", zap.Error(err))
+		}
 	}
 
 	// создаём сервис и внедряем репозиторий
 	shorter := service.NewShorterService(repo, cfg)
 
 	// создаём HTTP-сервер и внедряем сервис
-	server := handler.NewHTTPServer(cfg.BaseURL, shorter, zapLogger)
+	server := handler.NewHTTPServer(cfg.BaseURL, shorter, zapLogger, db)
 
 	fmt.Printf("Server run on: http://%s\n", cfg.RunAddr)
 	if err := http.ListenAndServe(cfg.RunAddr, server.Router()); err != nil {
-		log.Fatal(err)
+		zapLogger.Fatal("server stopped", zap.Error(err))
 	}
 }
