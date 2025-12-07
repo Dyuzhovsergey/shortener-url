a23e6f872445498d8bee623921aa89e61d9f99f2
Sergey <dyuzhovsergey@gmail.com>
2025-12-07 19:42:59 +0300

    add struct error exist originalURL

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -8,11 +8,3 @@ type Repository interface {
 	Save(ctx context.Context, shortID, originalURL string) error
 	Get(ctx context.Context, shortID string) (string, bool)
 }
-
-type ErrOriginalAlreadyExists struct {
-	ShortID string
-}
-
-func (e *ErrOriginalAlreadyExists) Error() string {
-	return "original URL already exists"
-}
